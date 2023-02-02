8620cf23d193f89ee40a6f5bdd9a056d5d67303a
Weed <39092862+moodoa@users.noreply.github.com>
2023-02-02 10:18:28 +0800

    <feat>main & dependency

---
+++ googlemap.py
@@ -0,0 +1,42 @@
+import googlemaps
+
+
+class GOOGLEMAP:
+    def __init__(self):
+        self.gmaps = googlemaps.Client(key="YOUR_GOOGLE_MAP_API_KEY")
+
+    def get_shop(self, user_location, category, user_radius):
+        output = ""
+        geocode_result = self.gmaps.geocode(user_location)
+        loc = geocode_result[0]["geometry"]["location"]
+        output += f"{user_location}為中心{str(user_radius)}公尺的{category}店家: \n\n"
+        shops = self.gmaps.places_nearby(
+            keyword=category, location=loc, radius=user_radius, language="zh-TW"
+        )
+        for shop in sorted(shops["results"], key=lambda x: x["rating"], reverse=True):
+            output += "🥢" + (shop["name"]) + "\n"
+            output += self._get_shop_status(shop)
+            output += f'評分：{(shop["rating"])}\n'
+            output += f"電話：{self._get_shop_number(shop)}\n"
+            output += f"地址：{shop['vicinity']}\n\n"
+        return output
+
+    def _get_shop_status(self, shop):
+        status = "休息中\n"
+        try:
+            opening = shop["opening_hours"]["open_now"]
+            if opening:
+                status = "營業中\n"
+        except:
+            status = "待確認\n"
+        return status
+
+    def _get_shop_number(self, shop):
+        number = "待確認"
+        try:
+            number = self.gmaps.place(place_id=shop["place_id"], language="zh-TW")[
+                "result"
+            ]["formatted_phone_number"]
+        except:
+            pass
+        return number
---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from googlemap import GOOGLEMAP
+import requests
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    # app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print(
+            "Invalid signature. Please check your channel access token/channel secret."
+        )
+        abort(400)
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    keyword = event.message.text.lower()
+    gmap = GOOGLEMAP()
+    if keyword.startswith("找店家") and "/" in keyword:
+        targets = keyword.split("/")
+        location = targets[1]
+        category = targets[2]
+        radius = targets[3]
+        output = gmap.get_shop(location, category, int(radius))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=output))
+
+
+if __name__ == "__main__":
+    app.run()
